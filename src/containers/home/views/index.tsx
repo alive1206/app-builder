c7a15d48f1e1acb18327c1d42bd87fff05f7a28b
Thang Le <thanglebao126@gmail.com>
2024-12-31 18:32:54 +0700

    fix: drag animation to another container

--- src/containers/home/views/index.tsx
+++ src/containers/home/views/index.tsx
@@ -7,6 +7,7 @@ import {
   DndContext,
   DragEndEvent,
   DragMoveEvent,
+  DragOverlay,
   DragStartEvent,
   KeyboardSensor,
   PointerSensor,
@@ -62,6 +63,14 @@ export const HomeScreen = () => {
     }
   };
 
+  const findItemTitle = (id: UniqueIdentifier | undefined) => {
+    const container = findValueOfItems(id, "item");
+    if (!container) return "";
+    const item = container.items.find((item) => item.id === id);
+    if (!item) return "";
+    return item.title;
+  };
+
   //Dnd Handlers
   const sensors = useSensors(
     useSensor(PointerSensor),
@@ -77,7 +86,7 @@ export const HomeScreen = () => {
   const handleDragMove = (event: DragMoveEvent) => {
     const { active, over } = event;
 
-    //Handle Items Sorting
+    // Handle Items Sorting
     if (
       active.id.toString().includes("item") &&
       over?.id.toString().includes("item") &&
@@ -85,14 +94,14 @@ export const HomeScreen = () => {
       over &&
       active.id !== over.id
     ) {
-      //Find the active container and over container
-      const activeContainer = findValueOfItems(active.id, " item");
-      const overContainer = findValueOfItems(over.id, " item");
+      // Find the active container and over container
+      const activeContainer = findValueOfItems(active.id, "item");
+      const overContainer = findValueOfItems(over.id, "item");
 
-      //If the active or over container is undefined, return
+      // If the active or over container is not found, return
       if (!activeContainer || !overContainer) return;
 
-      //Find the active and over container index
+      // Find the index of the active and over container
       const activeContainerIndex = containers.findIndex(
         (container) => container.id === activeContainer.id
       );
@@ -100,55 +109,55 @@ export const HomeScreen = () => {
         (container) => container.id === overContainer.id
       );
 
-      //Find the active and over item index
-      const activeItemIndex = activeContainer.items.findIndex(
+      // Find the index of the active and over item
+      const activeitemIndex = activeContainer.items.findIndex(
         (item) => item.id === active.id
       );
-      const overItemIndex = overContainer.items.findIndex(
+      const overitemIndex = overContainer.items.findIndex(
         (item) => item.id === over.id
       );
-
-      //In the same container
+      // In the same container
       if (activeContainerIndex === overContainerIndex) {
         const newItems = [...containers];
         newItems[activeContainerIndex].items = arrayMove(
           newItems[activeContainerIndex].items,
-          activeItemIndex,
-          overItemIndex
+          activeitemIndex,
+          overitemIndex
         );
+
         setContainers(newItems);
       } else {
-        //In different container
+        // In different containers
         const newItems = [...containers];
-        const [removedItem] = newItems[activeContainerIndex].items.splice(
-          activeItemIndex,
+        const [removeditem] = newItems[activeContainerIndex].items.splice(
+          activeitemIndex,
           1
         );
         newItems[overContainerIndex].items.splice(
-          overItemIndex,
+          overitemIndex,
           0,
-          removedItem
+          removeditem
         );
         setContainers(newItems);
       }
     }
 
-    //Handle item drop into a container
+    // Handling Item Drop Into a Container
     if (
       active.id.toString().includes("item") &&
-      over?.id.toString().includes("item") &&
+      over?.id.toString().includes("container") &&
       active &&
       over &&
       active.id !== over.id
     ) {
-      //Find the active container and over container
-      const activeContainer = findValueOfItems(active.id, " item");
-      const overContainer = findValueOfItems(over.id, " item");
+      // Find the active and over container
+      const activeContainer = findValueOfItems(active.id, "item");
+      const overContainer = findValueOfItems(over.id, "container");
 
-      //If the active or over container is undefined, return
+      // If the active or over container is not found, return
       if (!activeContainer || !overContainer) return;
 
-      //Find the active and over container index
+      // Find the index of the active and over container
       const activeContainerIndex = containers.findIndex(
         (container) => container.id === activeContainer.id
       );
@@ -156,18 +165,18 @@ export const HomeScreen = () => {
         (container) => container.id === overContainer.id
       );
 
-      //Find the active and over item index
-      const activeItemIndex = activeContainer.items.findIndex(
+      // Find the index of the active and over item
+      const activeitemIndex = activeContainer.items.findIndex(
         (item) => item.id === active.id
       );
 
-      //Remove the active item from the active container and add it to the over container
+      // Remove the active item from the active container and add it to the over container
       const newItems = [...containers];
-      const [removedItem] = newItems[activeContainerIndex].items.splice(
-        activeItemIndex,
+      const [removeditem] = newItems[activeContainerIndex].items.splice(
+        activeitemIndex,
         1
       );
-      newItems[overContainerIndex].items.push(removedItem);
+      newItems[overContainerIndex].items.push(removeditem);
       setContainers(newItems);
     }
   };
@@ -298,6 +307,12 @@ export const HomeScreen = () => {
                 </Container>
               ))}
             </SortableContext>
+            <DragOverlay adjustScale={false}>
+              {/* Drag Overlay For item Item */}
+              {activeId && activeId.toString().includes("item") && (
+                <Items id={activeId} title={findItemTitle(activeId)} />
+              )}
+            </DragOverlay>
           </DndContext>
         </div>
       </div>
